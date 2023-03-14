955f7059a1b8d1f3f8d2b39e1a3a76bd8ce0f6ac
ChangCL <chang@cheeloy.com>
2023-03-14 19:01:48 +0800

    fedora update

--- keymap-with-caps-lock.c
+++ keymap-with-caps-lock.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TD(FAT_ARROW), KC_1, KC_2, KC_3, KC_4, KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TD(ARROW_OP),
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   LT(_MY_RGB, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_SFTENT,
+  TD(SHIFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_SFTENT,
   LT(_MY_FUNCTION, KC_DEL), KC_LCTL, KC_LALT, KC_LGUI, LOWER, SFTSPC, KC_SPC, RAISE, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
